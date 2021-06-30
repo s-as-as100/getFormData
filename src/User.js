9497675a0e239f7665377f76c3d082686364c68c
s-as-as100 <s.arifsaifi@gmail.com>
2021-06-30 18:38:32 +0530

    pass function as props

--- src/User.js
+++ src/User.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-export default function user(props) {
-     const {data} = props;
-    return (
-        <div>
-            <h1>user component</h1>
-            <h4>{data.name}</h4>
-            <h4>{data.age}</h4>
-        </div>
-    )
+export default function User(props) {
+     
+     return(
+         <div>
+             <button onClick = {props.data} >function pass as props</button>
+         </div>
+     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
  import React from 'react'
 import { useState } from 'react'
+import User from '../User'
  
 
 
@@ -8,6 +9,14 @@ import { useState } from 'react'
      const [name,setName] = useState('')
      const [option,setOption] =useState("")
      const [chec, setChec]  = useState(false)
+     const[loggedIn,setLoggedIn] = useState(true)
+
+    // pass function as props 
+    function alt() {
+        alert(" function pass as porps")
+    }
+
+
 
     // prevent form submit button data
    function getFormData(e) {
@@ -17,8 +26,12 @@ import { useState } from 'react'
 
      return (
          <div style={{position:"absolute", top:"10%", left:"40%"}}>
+                {
+                    loggedIn? <h1> please login</h1> : <h2> no login</h2>
+                }
+
              <h1>handle react form</h1>
-           <form onSubmit={getFormData}>
+           <form onSubmit={getFormData} value={name} >
                <input onChange={(e) =>setName(e.target.value)} type="text" placeholder="name"/> <br></br>
                <select onChange={(e) => setOption(e.target.value)} >
                    <option>name</option>
@@ -31,8 +44,11 @@ import { useState } from 'react'
                <input onChange ={(e) =>setChec(e.target.checked)} type="checkbox" /> <span>Accept terms and conditions</span>
                <br></br>
                <button type="submit"> Submit </button>
+               <button  > clear </button>
+
 
            </form>
+           <User data={alt} />
 
 
 
